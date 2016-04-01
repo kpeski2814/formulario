b1c92054f3f9f99b8192a75178fd9697cdea544c
ronald <kpeski2814@hotmail.com>
2016-03-31 22:08:20 -0500

    empezando el proyecto

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Este es un formulario </title>
+    <link rel="stylesheet" href="css/master.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+
+    <form  action="index.php" method="post">
+
+  <fieldset>
+<legend>Datos Personales</legend>
+<div class="">
+<label for="">Nombre:</label>
+<input type="text" name="name" value="">
+</div>
+
+<div class="">
+<label for="">Edad:</label>
+<input type="text" name="name" value="" autofocus>
+</div>
+<div id="container">
+   <ul>
+      <li> List Item
+        <ul>
+           <li> Child </li>
+        </ul>
+      </li>
+      <li> List Item </li>
+      <li> List Item </li>
+      <li> List Item </li>
+   </ul>
+</div>
+<div>
+<input type="submit" name="name" value="Validar">
+</div>
+
+  </fieldset>
+
+    </form>
+<a href="http://html5facil.com/tutoriales/validacion-formularios-nativos-html5/">ir a google</a>
+  </body>
+</html>
